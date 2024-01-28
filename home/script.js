bdd29ebf1e4cb0ae8105569662ecdbbc590afc8f
Thisizasif <68771679+thisizasif@users.noreply.github.com>
2024-01-28 19:05:01 +0530

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,9 +1,1 @@
-// JavaScript for dropdown menu
-function myFunction() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
+
